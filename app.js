0821e29ea8e126b21f40aa5f8252723d2c8407ab
Mai Campbell <jirallc@gmail.com>
2019-10-24 18:44:39 -0700

    completed calculator functionalities

--- app.js
+++ app.js
@@ -4,27 +4,84 @@ class Calculator {
         this.currentInput = currentInput;
         this.clear();
     }
+
     clear() {
         this.currentOperand = '';
         this.previousOperand = '';
         this.operation = undefined;
     }
+
     delete() {
         this.currentOperand = this.currentOperand.slice(0, -1);
-
     }
+
     compute() {
+        let computation;
+        let previous = parseFloat(this.previousOperand);
+        let current = parseFloat(this.currentOperand);
+        if(isNaN(previous) || isNaN(current)) return;
 
+        switch(this.operation) {
+            case "+":
+                computation = previous + current;
+                break;
+                case "-":
+                computation = previous - current;
+                break;
+                case "x":
+                    computation = previous * current;
+                    break;
+                    case "÷":
+                    computation = previous / current;
+                    break;
+                    default:
+                        return;
+        }
+        this.currentOperand = computation
+        this.operation = undefined;
+        this.previousOperand = '';
     }
+
     chooseOperation(operation) {
-
+        if(this.currentOperand === '') return;
+        if(this.previousOperand === '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     }
+
     appendNumber(number ) {
+        if(number === '.' && this.currentOperand.includes('.')) return;
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
-    updateResult() {
-        this.currentInput.innerText = this.currentOperand;
 
+    getNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigit = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigit = stringNumber.split('.')[1];
+        let displayInteger;
+        if(isNaN(integerDigit)) {
+            displayInteger = '';
+        } else {
+            displayInteger = integerDigit.toLocaleString('en-US', {maximumFractionDigits: 0});
+        }
+
+        if(decimalDigit != null) {
+            return `${integerDigit}.${decimalDigit}`
+        } else {
+            return displayInteger;
+
+        }
+    }
+
+    updateResult() {
+        this.currentInput.innerText = this.getNumber(this.currentOperand)
+        if(this.operation != null) {
+            this.previousInput.innerText = `${this.getNumber(this.previousOperand)} ${this.operation}`;
+        }
+          
     }
 }
 
